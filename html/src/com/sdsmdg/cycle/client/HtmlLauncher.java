88044904d529dd3d8d236e79d72c016b63018ba7
Benjamin Schulte <bs@golfgl.de>
2019-07-04 21:11:00 +0200

    Preload Logo

--- html/src/com/sdsmdg/cycle/client/HtmlLauncher.java
+++ html/src/com/sdsmdg/cycle/client/HtmlLauncher.java
@@ -3,6 +3,10 @@ package com.sdsmdg.cycle.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.Panel;
 import com.sdsmdg.cycle.AboutUs;
 import com.sdsmdg.cycle.CGame;
 
@@ -13,7 +17,7 @@ public class HtmlLauncher extends GwtApplication {
 
     @Override
     public GwtApplicationConfiguration getConfig() {
-        GwtApplicationConfiguration gwtApplicationConfiguration = new GwtApplicationConfiguration(640, 960);
+        GwtApplicationConfiguration gwtApplicationConfiguration = new GwtApplicationConfiguration(480, 720);
         gwtApplicationConfiguration.usePhysicalPixels = true;
         return gwtApplicationConfiguration;
     }
@@ -21,5 +25,19 @@ public class HtmlLauncher extends GwtApplication {
     @Override
     public ApplicationListener createApplicationListener() {
         return new CGame(new KongClient(), null);
+    }
+
+    @Override
+    public Preloader.PreloaderCallback getPreloaderCallback() {
+        return createPreloaderPanel(GWT.getHostPageBaseURL() + "preload.png");
+    }
+
+    @Override
+    protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+        meterPanel.setStyleName("gdx-meter");
+        meterPanel.addStyleName("nostripes");
+        Style meterPanelStyle = meterPanel.getElement().getStyle();
+        meterStyle.setProperty("backgroundColor", "#ff0000");
+        meterStyle.setProperty("backgroundImage", "none");
     }
 }
